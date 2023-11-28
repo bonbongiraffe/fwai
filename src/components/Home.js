e150d767e79d0d913825979967460a7ec3eebfc4
bonbongiraffe <francescowai@yahoo.com>
2023-11-28 16:14:15 -0500

    basic setup and tailwind setup

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+function Home(){
+    return (
+        <div>
+            <h1>Francesco Wai</h1>
+            <ul>
+                <li>Software Engineering Professional</li>
+                <li>Bartender at Home</li>
+                <li>Nerd at Heart</li>
+            </ul>
+            <a href='https://github.com/bonbongiraffe'>GitHub</a>
+            <a href='https://www.linkedin.com/in/francescowai/'>LinkedIn</a>
+            <a href='https://drive.google.com/file/d/149yaqkFjO2P507j_SrAKw38QQGMt2Fh7/view?usp=sharing'>Resume</a>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+function NavBar(){
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+            Welcome to my personal site
+        </div>
+    )
+}
+
+export default NavBar
