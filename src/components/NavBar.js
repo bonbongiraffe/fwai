839f2720ed92a11e0c8939a0fdcd8dcfc1857bee
bonbongiraffe <francescowai@yahoo.com>
2023-11-29 13:26:27 -0500

    photo added, home block wip

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,12 @@
+import photo1 from '../assets/img-01.JPG'
+
 function NavBar(){
     return (
         <div>
-            <h1 className="text-3xl font-bold underline">
-                 Hello world!
+            <h1 className="text-3xl font-bold">
+                 Francesco Wai
             </h1>
-            Welcome to my personal site
+            <img src={photo1} alt='photo 1' className='h-1/3 w-1/3'/>
         </div>
     )
 }
