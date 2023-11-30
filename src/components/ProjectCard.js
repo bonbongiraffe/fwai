264ecf332034d20fb02119fdad94ee6c46a6b9fc
bonbongiraffe <francescowai@yahoo.com>
2023-11-30 15:10:53 -0500

    factor out svg, add youtube and github icons to project card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,11 +1,20 @@
+import { ReactComponent as GitHubIcon } from '../icons/github.svg'
+import { ReactComponent as YouTubeIcon } from '../icons/youtube.svg'
+
 function ProjectCard({ project }){
     return(
         <div>
             <img className='h-20 w-20' src={process.env.PUBLIC_URL+`/assets/${project.image}`} alt={project.name}/>
             <p>{project.name}</p>
             <p>{project.description}</p>
-            <p>{project.github}</p>
-            <p>{project.youtube}</p>
+            <a href={project.github}>
+                <span className='sr-only'>github link</span>
+                <GitHubIcon className='h-6 w-6'/>
+            </a>
+            <a href={project.youtube}>
+                <span className='sr-only'>youtube link</span>
+                <YouTubeIcon className='h-6 w-6'/>
+            </a>
             <p>{project.technologies}</p>
         </div>
     )
