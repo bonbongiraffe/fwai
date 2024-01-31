273dda0d599c8e28ae679c2bae9a16ce5ed46ff6
bonbongiraffe <francescowai@yahoo.com>
2024-01-31 18:29:28 -0500

    31-Jan end of day, smtp server connection wip

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, make_response
 from flask_cors import CORS
 import smtplib
 from email.mime.text import MIMEText
@@ -13,32 +13,35 @@ CORS(app)
 def send_email():
     try:
         data = request.json
-        name = data.get('name,','')
+        name = data.get('name','')
         email = data.get('email','')
         message = data.get('message','')
 
         #configure email server and credentials
-        smtp_server = 
-        smtp_port = 465
-        smtp_username = 
-        smtp_password = ''
+        smtp_server = os.environ.get('SMTP_SERVER')
+        smtp_port = int(os.environ.get('SMTP_PORT'))
+        smtp_username = os.environ.get('SMTP_USERNAME')
+        smtp_password = os.environ.get('SMTP_PASSWORD')
+        email_destination = os.environ.get('EMAIL')
 
         #create MIMEText object for email
         msg = MIMEMultipart()
-        msg['From'] = smtp_username
-        msg['To'] = ''
-        msg['Subject'] = f''
+        msg['From'] = email_destination
+        msg['Subject'] = f'test'
         msg.attach(MIMEText(f'Name: {name}\nEmail: {email}\nMessage: {message}', 'plain'))
 
         #create and start smtp server connection
         server = smtplib.SMTP(smtp_server, smtp_port)
-        server.starttls()
+        server.starttls(context=ssl.create_default_context(ssl.Purpose.CLIENT_AUTH))
+        server.set_debuglevel(1)
 
         #login to smtp server
         server.login(smtp_username, smtp_password)
 
+        return make_response({'test':'good!'},200)
+
         #send the email
-        server.sendmail(smtp_username, 'recipient-email@example.com', msg.as_string())
+        server.sendmail(smtp_username, email_destination, msg.as_string())
 
         #quit smtp server
         server.quit()
@@ -47,8 +50,6 @@ def send_email():
     except Exception as e:
         print('Error sending email:', str(e))
         return make_response({'success': False, 'error': 'Internal Server Error'}, 500)
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
